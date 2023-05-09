e7ef3245ab61fe07b693003bc6d212da6a5e9d8a
Cezary Stanislawski <cezary.marek.stanislawski@gmail.com>
2023-05-09 09:55:56 +0200

    Created 3 versions of .gitlab-ci.yml
    Each contain:
    - defined 5 stages, each containing 1 job, as they're independent of
      each other
    - set of commands to set npm config, variables, login to registry & install dependencies, all based on package.json content
    - image to use for the pipeline, based on the Node.js version in package.json. Since it can't be read using node's print, we need to install & run jq to extract the version from package.json
    - verify version job, that checks: 1. If the current package version is newer than the one published to the registry, 2. If the current package version is newer than the one in the main branch
    - 3 script jobs: test, lint, build
    - publish job publishing to https://extra-npm.com
    
    difference in minimal version happens in .image (which isn't present in
    the minimal solution) & in .set_node_version. All the behavior has been
    moved to .set_node_version.
    
    difference in cache is in lines 30-39 & 113-117.

---
+++ read_yaml.py
@@ -0,0 +1,15 @@
+from yaml import load, Loader
+
+def read_yaml(filepath: str) -> dict:
+    with open(filepath, 'r') as f:
+        return load(f, Loader=Loader)
+
+if __name__ == '__main__':
+    gitlab_ci_config = read_yaml('.gitlab-ci.yml')
+    # print keys & their values
+    print()
+    for key, value in gitlab_ci_config.items():
+        print(key + ':')
+        print(value)
+        print()
+        print()
